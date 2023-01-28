a4021fea587137270f31cd746c3870b2ca80ea95
Ali Ajeli <mr.ali.ajeli@hotmail.com>
2023-01-28 15:47:55 +0330

    Add sent enail for contact form

--- app/contact/ContactForm.tsx
+++ app/contact/ContactForm.tsx
@@ -18,20 +18,32 @@ const ContactForm = () => {
 
   return (
     <form
-      onSubmit={handleSubmit(onSubmit)}
+      // onSubmit={handleSubmit(onSubmit)}
       className="flex flex-col p-5 md:w-full md:py-10 md:gap-2"
+      action="https://formsubmit.co/mr.ali.ajeli@hotmail.com"
+      method="POST"
     >
+      {/* Honeypot */}
+      <input type="text" name="_honey" className="hidden" />
+      {/* Disable Captcha */}
+      <input type="hidden" name="_captcha" value="false" />
+      {/* Success Page */}
+      <input
+        type="hidden"
+        name="_next"
+        value="http://aliajeli.ir/contact/success"
+      />
+      {/* template */}
+      <input type="hidden" name="_template" value="box" />
       <input
         defaultValue=""
         placeholder="Whats your name ?"
         {...register("name", { required: true })}
         className="p-2 rounded-lg text-accent outline outline-[3px] focus:outline-accent dark:focus:outline-accent placeholder:text-accent/50 placeholder:text-sm bg-light dark:bg-dark dark:outline-light/50 outline-dark/50"
       />
-
       <span className="h-6 px-3 grid items-center text-xs text-red-500">
         {errors.name && "Please enter your Name"}
       </span>
-
       <input
         type="email"
         defaultValue=""
@@ -39,27 +51,24 @@ const ContactForm = () => {
         {...register("email", { required: true })}
         className="p-2 rounded-lg text-accent text-sm outline outline-[3px] focus:outline-accent dark:focus:outline-accent placeholder:text-accent/50 placeholder:text-sm bg-light dark:bg-dark dark:outline-light/50 outline-dark/50"
       />
-
       <span className="h-6 px-3 grid items-center text-xs text-red-500">
         {errors.email && "Please enter your Email Address"}
       </span>
-
       <textarea
         placeholder="What can i do for you ?"
         rows={8}
         {...register("message", { required: true })}
         className="p-2 rounded-lg text-accent outline outline-[3px] focus:outline-accent dark:focus:outline-accent placeholder:text-accent/50 placeholder:text-sm bg-light dark:bg-dark dark:outline-light/50 outline-dark/50 "
       />
-
       <span className="h-6 px-3 grid items-center text-xs text-red-500">
         {errors.message && "Please enter a message"}
       </span>
-
-      <input
+      <button
         type="submit"
-        value="Send me your message"
         className="bg-accent p-5 rounded-lg text-light dark:text-dark border-4 border-accent cursor-pointer dark:hover:bg-dark dark:hover:text-accent hover:bg-light hover:text-accent"
-      />
+      >
+        Send me your message
+      </button>
     </form>
   );
 };
